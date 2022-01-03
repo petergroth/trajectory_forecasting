5385012552cd0310202195bc8ebc9c90fb895c15
Peter Groth <petermoerchgroth@gmail.com>
2022-01-03 19:50:43 +0100

    restructuring nbody scripts

---
+++ src/predictions/evaluate_nbody.py
@@ -0,0 +1,54 @@
+import argparse
+import os
+
+import hydra
+import matplotlib.pyplot as plt
+import numpy as np
+import pytorch_lightning as pl
+import torch
+import yaml
+# from models import ConstantModel
+from matplotlib.patches import Circle, Rectangle, Ellipse
+import matplotlib.colors as colors
+from omegaconf import DictConfig, OmegaConf
+from pytorch_lightning.utilities.seed import seed_everything
+
+from src.data.dataset_nbody import OneStepNBodyDataModule, SequentialNBodyDataModule
+from src.training_modules.train_nbody_model import *
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("config")
+    parser.add_argument("--ckpt_path", default=None)
+    args = parser.parse_args()
+
+    # Load config file
+    with open(args.config) as f:
+        config = yaml.safe_load(f)
+
+    # Set seed
+    seed_everything(config["misc"]["seed"], workers=True)
+    # Load datamodule
+    config["datamodule"]["val_batch_size"] = 64
+    config["datamodule"]["shuffle"] = False
+    datamodule = eval(config["misc"]["dm_type"])(**config["datamodule"])
+
+    # Load correct model
+    if config["misc"]["model_type"] != "ConstantModel":
+        regressor = eval(config["misc"]["regressor_type"]).load_from_checkpoint(args.ckpt_path)
+        regressor.eval()
+    else:
+        regressor = ConstantPhysicalBaselineModule(prediction_horizon=51)
+
+    # Load trainer
+    trainer = pl.Trainer(**config["trainer"])
+    trainer.validate(regressor, ckpt_path=args.ckpt_path, datamodule=datamodule)
+
+    # Test
+    trainer.test(regressor, datamodule=datamodule, ckpt_path=args.ckpt_path)
+
+
+if __name__ == "__main__":
+    main()
+
--- src/predictions/make_nbody_predictions.py
+++ src/predictions/make_nbody_predictions.py
@@ -4,6 +4,8 @@ import os
 import hydra
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib import rc
+from matplotlib.patches import Circle
 import pytorch_lightning as pl
 import torch
 import yaml
@@ -13,15 +15,28 @@ from omegaconf import DictConfig, OmegaConf
 from pytorch_lightning.utilities.seed import seed_everything
 
 from src.data.dataset_nbody import *
-from src.models.train_nbody_model import *
+from src.training_modules.train_nbody_model import (OneStepModule,
+                                                    SequentialModule,
+                                                    ConstantPhysicalBaselineModule)
+
+rc("text", usetex=True)
+rc("font", **{"family": "serif", "serif": ["Computer Modern Roman"]})
+
+ticksize = 30
+titlesize = 35
+plt.rcParams["axes.labelsize"] = titlesize
+plt.rcParams["axes.titlesize"] = titlesize
+plt.rcParams["xtick.labelsize"] = ticksize
+plt.rcParams["ytick.labelsize"] = ticksize
 
 
-def make_predictions(path, config, sequence_idx=0):
+def make_predictions(path: str, config: dict, n_steps: int = 51, sequence_idx: int = 0):
     # Set seed
     seed_everything(config["misc"]["seed"], workers=True)
     # Load datamodule
     config["datamodule"]["val_batch_size"] = 1
     config["datamodule"]["batch_size"] = 1
+    config["datamodule"]["shuffle"] = False
     datamodule = eval(config["misc"]["dm_type"])(**config["datamodule"])
 
     # Load correct model
@@ -32,15 +47,15 @@ def make_predictions(path, config, sequence_idx=0):
     # Setup
     regressor.eval()
     datamodule.setup()
-    loader = datamodule.val_dataloader()
-    # Define output path
-    dirpath = "src/predictions/raw_preds/nbody/" + config["logger"]["version"]
-    os.makedirs(dirpath, exist_ok=True)
-    for i, batch in enumerate(loader):
-        if i == sequence_idx:
-            y_hat, y_target = regressor.predict_step(batch)
-            torch.save((y_hat, y_target), dirpath + f"/sequence_{i:04}.pt")
-            return
+    dataset = datamodule.val_dataset
+    # Extract batch and add missing attributes
+    batch = dataset.__getitem__(sequence_idx)
+    batch.batch = torch.zeros(batch.x.size(0)).type(torch.int64)
+    batch.num_graphs = 1
+
+    # Make and return predictions
+    y_hat, y_target = regressor.predict_step(batch, prediction_horizon=n_steps)
+    return y_hat.detach(), y_target
 
 
 def main():
@@ -56,133 +71,122 @@ def main():
         config = yaml.safe_load(f)
 
     # Computes predictions for specified sequence
-    make_predictions(
+    y_hat, y_target = make_predictions(
         path=args.ckpt_path,
         config=config,
         sequence_idx=args.sequence_idx,
     )
 
-    # Create directory for current model
-    # vis_dir = "visualisations/predictions/waymo/" + config["logger"]["version"] + "/"
-    os.makedirs(args.output_path, exist_ok=True)
-    # Location of prediction files
-    dir = "src/predictions/raw_preds/nbody/" + config["logger"]["version"] + "/"
-    # Load first file in directory
-    path = "sequence_" + f"{args.sequence_idx:04}.pt"
-    y_hat, y_target = torch.load(dir + path)
-    y_hat = y_hat.detach()
     n_steps, n_agents, n_features = y_hat.shape
+
+    # Convert to numpy arrays
+    y_hat = y_hat.detach().numpy()
+    y_target = y_target.detach().numpy()
+
+
     # Extract boundaries
     x_min, x_max, y_min, y_max = (
-        torch.min(y_target[:, :, 0]).item(),
-        torch.max(y_target[:, :, 0]).item(),
-        torch.min(y_target[:, :, 1]).item(),
-        torch.max(y_target[:, :, 1]).item(),
+        np.min(y_target[:, :, 0]) - 10,
+        np.max(y_target[:, :, 0]) + 10,
+        np.min(y_target[:, :, 1]) - 10,
+        np.max(y_target[:, :, 1]) + 10,
     )
 
-    figglob, axglob = plt.subplots(1, 2, figsize=(20, 10))
+    # Setup plotting
+    alphas = np.linspace(0.1, 1, n_steps)
+    fig, ax = plt.subplots(1, 2, figsize=(20, 10))
 
     colors = [
-        (np.random.random(), np.random.random(), np.random.random())
-        for _ in range(n_agents)
+        "darkorange",
+        "salmon",
+        "sandybrown",
+        "chocolate",
+        "crimson",
+        "darkred",
+        "orangered",
+        "orange",
+        "peru",
+        "peru",
     ]
-    for agent in range(n_agents):
-        # color = (np.random.random(), np.random.random(), np.random.random())
-        x = y_target[:, agent, 0].detach().numpy()
-        y = y_target[:, agent, 1].detach().numpy()
-        axglob[0].scatter(
-            x=x, y=y, s=50, color=colors[agent], alpha=0.2, edgecolors="k"
-        )
-        axglob[0].quiver(
-            y_target[0, agent, 0].detach().numpy(),
-            y_target[0, agent, 1].detach().numpy(),
-            y_target[0, agent, 2].detach().numpy(),
-            y_target[0, agent, 3].detach().numpy(),
-            width=0.003,
-            headwidth=5,
-            angles="xy",
-            scale_units="xy",
-            scale=1.0,
-            color="lightgrey",
-        )
-        axglob[0].quiver(
-            y_target[10, agent, 0].detach().numpy(),
-            y_target[10, agent, 1].detach().numpy(),
-            y_target[10, agent, 2].detach().numpy(),
-            y_target[10, agent, 3].detach().numpy(),
-            width=0.003,
-            headwidth=5,
-            angles="xy",
-            scale_units="xy",
-            scale=1.0,
-            color="gray",
-        )
-        axglob[0].quiver(
-            y_target[n_steps - 1, agent, 0].detach().numpy(),
-            y_target[n_steps - 1, agent, 1].detach().numpy(),
-            y_target[n_steps - 1, agent, 2].detach().numpy(),
-            y_target[n_steps - 1, agent, 3].detach().numpy(),
-            width=0.003,
-            headwidth=5,
-            angles="xy",
-            scale_units="xy",
-            scale=1.0,
-            color="k",
-        )
 
-        x = y_hat[:, agent, 0].detach().numpy()
-        y = y_hat[:, agent, 1].detach().numpy()
-        axglob[1].scatter(
-            x=x, y=y, s=30, color=colors[agent], alpha=0.2, edgecolors="k"
-        )
-        axglob[1].quiver(
-            y_hat[0, agent, 0].detach().numpy(),
-            y_hat[0, agent, 1].detach().numpy(),
-            y_hat[0, agent, 2].detach().numpy(),
-            y_hat[0, agent, 3].detach().numpy(),
-            width=0.003,
-            headwidth=5,
-            angles="xy",
-            scale_units="xy",
-            scale=1.0,
-            color="lightgrey",
-        )
-        axglob[1].quiver(
-            y_hat[10, agent, 0].detach().numpy(),
-            y_hat[10, agent, 1].detach().numpy(),
-            y_hat[10, agent, 2].detach().numpy(),
-            y_hat[10, agent, 3].detach().numpy(),
-            width=0.003,
-            headwidth=5,
-            angles="xy",
-            scale_units="xy",
-            scale=1.0,
-            color="gray",
-        )
-        axglob[1].quiver(
-            y_hat[n_steps - 1, agent, 0].detach().numpy(),
-            y_hat[n_steps - 1, agent, 1].detach().numpy(),
-            y_hat[n_steps - 1, agent, 2].detach().numpy(),
-            y_hat[n_steps - 1, agent, 3].detach().numpy(),
-            width=0.003,
-            headwidth=5,
-            angles="xy",
-            scale_units="xy",
-            scale=1.0,
-            color="k",
-        )
+    # Extract target positions and sizes
+    positions = y_target[:, :, :2]
+    velocities = y_target[:, :, 2:4]
+    sizes = y_target[:, :, 4]
+    for t in range(n_steps):
+        for i in range(n_agents):
+            # Visualise each agent as a circle
+            ax[0].add_patch(
+                Circle(
+                    positions[t, i],
+                    sizes[t, i],
+                    facecolor=colors[i],
+                    alpha=alphas[t],
+                    edgecolor='k' if t in (n_steps - 1, 10) else None
+                )
+            )
 
-    axglob[0].axis("equal")
-    axglob[0].set_xlim((x_min, x_max))
-    axglob[0].set_ylim((y_min, y_max))
-    axglob[1].axis("equal")
-    axglob[1].set_xlim((x_min, x_max))
-    axglob[1].set_ylim((y_min, y_max))
-    axglob[0].set_title("Groundtruth trajectories")
-    axglob[1].set_title("Predicted trajectories")
+    # Show final velocities
+    ax[0].quiver(
+        positions[n_steps-1, :, 0],
+        positions[n_steps-1, :, 1],
+        velocities[n_steps-1, :, 0],
+        velocities[n_steps-1, :, 1],
+        width=0.003,
+        headwidth=5,
+        angles="xy",
+        scale_units="xy",
+        scale=1.0,
+        color="k",
+        zorder=2,
+    )
 
-    plt.show()
-    figglob.savefig(f"{args.output_path}/sequence_{args.sequence_idx:04}.png")
+    # Extract target positions and sizes
+    positions = y_hat[:, :, :2]
+    velocities = y_hat[:, :, 2:4]
+    sizes = y_hat[:, :, 4]
+    for t in range(n_steps):
+        for i in range(n_agents):
+            # Visualise each agent as a circle
+            ax[1].add_patch(
+                Circle(
+                    positions[t, i],
+                    sizes[t, i],
+                    facecolor=colors[i],
+                    alpha=alphas[t],
+                    edgecolor='k' if t in (n_steps - 1, 10) else None
+                )
+            )
+
+    # Show final velocities
+    ax[1].quiver(
+        positions[n_steps-1, :, 0],
+        positions[n_steps-1, :, 1],
+        velocities[n_steps-1, :, 0],
+        velocities[n_steps-1, :, 1],
+        width=0.003,
+        headwidth=5,
+        angles="xy",
+        scale_units="xy",
+        scale=1.0,
+        color="k",
+    )
+
+
+
+    # ax[0].axis("equal")
+    # ax[0].set_xlim((x_min, x_max))
+    # ax[0].set_ylim((y_min, y_max))
+    # ax[1].axis("equal")
+    # ax[1].set_xlim((x_min, x_max))
+    # ax[1].set_ylim((y_min, y_max))
+    plt.setp(ax, xlim=(x_min, x_max), ylim=(y_min, y_max))
+    ax[0].set_title("Groundtruth trajectories", pad=8)
+    ax[1].set_title("Predicted trajectories", pad=8)
+
+    plt.tight_layout()
+    # plt.show()
+    plt.savefig(f"../../thesis/graphics/synthetic/constant_example.pdf")
 
 
 if __name__ == "__main__":
